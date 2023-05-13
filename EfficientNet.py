056f30214ea0a14b6f168068ebb6842cfafcb61a
tw-tychang <thomas.chang1688888@gmail.com>
2023-05-14 07:31:35 +0800

    [mod]EfficientNet.py,add some code

--- EfficientNet.py
+++ EfficientNet.py
@@ -116,20 +116,20 @@ class EffNetV2(nn.Module):
                 input_channel = output_channel
         self.features = nn.Sequential(*layers)
         # building last several layers
-        output_channel = (
+        self.output_channel = (
             _make_divisible(1792 * width_mult, 8) if width_mult > 1.0 else 1792
         )
-        self.conv = conv_1x1_bn(input_channel, output_channel)
+        self.conv = conv_1x1_bn(input_channel, self.output_channel)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Linear(output_channel, num_classes)
+        self.classifier = nn.Linear(self.output_channel, num_classes)
         self._initialize_weights()
 
     def forward(self, x):
         x = self.features(x)
         x = self.conv(x)
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.classifier(x)
         return x
 
     def _initialize_weights(self):
