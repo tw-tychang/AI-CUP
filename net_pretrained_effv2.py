2087af40f90aecf70877d6b417b32480984c167f
tw-tychang <thomas.chang1688888@gmail.com>
2023-05-15 04:47:03 +0800

    [mod]modify sone code

--- net_pretrained_effv2.py
+++ net_pretrained_effv2.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from torchvision.models.efficientnet import efficientnet_v2_m, EfficientNet_V2_M_Weights
+from torchvision.models.efficientnet import efficientnet_v2_m, EfficientNet_V2_M_Weights, EfficientNet
 from net import Conveter
 
 
@@ -11,20 +11,23 @@ def efficientnet_v2_m_forward(self, x: torch.Tensor) -> torch.Tensor:
     return x
 
 
+EfficientNet.forward = efficientnet_v2_m_forward
+
+
 class Net(nn.Module):
     def __init__(self, in_seq, output_classes, *args, **kwargs) -> None:
         super(Net, self).__init__(*args, **kwargs)
 
-        self.convert = Conveter(in_seq)
+        self.converter = Conveter(in_seq)
         self.features = efficientnet_v2_m(weights=EfficientNet_V2_M_Weights.IMAGENET1K_V1)
-        self.features.forward = efficientnet_v2_m_forward
+        # self.features.forward = efficientnet_v2_m_forward
         self.linear = nn.Sequential(
             nn.Linear(self.features.lastconv_output_channels, 1000),
             nn.Linear(1000, output_classes),
         )
 
-    def forward(self,x):
-        x = self.convert(x)
+    def forward(self, x):
+        x = self.converter(x)
         x = self.features(x)
         x = self.linear(x)
         return x
