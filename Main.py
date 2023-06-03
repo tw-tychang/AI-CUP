65a6738da86b91b07b378d418ef5b86c35d3aad0
tw-tychang <thomas.chang1688888@gmail.com>
2023-06-03 17:28:57 +0800

    [mod]Main.py, do argumention in gpu

--- Main.py
+++ Main.py
@@ -1,27 +1,26 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 import argparse
 import random
-import torch
 import numpy as np
-import torchvision
+import pandas as pd
+import torch
 import torchvision.transforms as transforms
-import torchvision.models as models
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import EfficientNet
 import warnings
 from data_parallel_my_v2 import BalancedDataParallel
 from Loss import CustomLoss
-from time import sleep
-from tqdm import tqdm, trange
+from tqdm import tqdm
 from src.data_process import DatasetInfo, get_dataloader, Processing, get_test_dataloader
-from src.transforms import CustomCompose, RandomCrop, RandomResizedCrop, RandomHorizontalFlip, RandomRotation
+from src.transforms import CustomCompose, IterativeCustomCompose, RandomCrop, RandomResizedCrop, RandomHorizontalFlip, RandomRotation
 from lib.FileTools.FileSearcher import get_filenames
-from net import BadminationNet
+from net_pretrained_effv2 import Net
+from lib.ML_Tools.ModelPerform import ModelPerform
+
 
 warnings.filterwarnings("ignore", category=UserWarning)
+
 
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -29,22 +28,21 @@ def main(args):
 
     # batch_size = args.b
 
-
     # argumentation
-    sizeHW = (640, 640)
+    sizeHW = (512, 512)
     argumentation_order_ls = [
+        RandomResizedCrop(sizeHW, scale=(0.6, 1.6), ratio=(3.0 / 5.0, 2.0), p=0.9),
         RandomHorizontalFlip(p=0.5),
         transforms.GaussianBlur([3, 3]),
-        transforms.RandomApply([transforms.ColorJitter(brightness=0.4, hue=0.2, contrast=0.5, saturation=0.2)], p=0.75),
-        transforms.RandomPosterize(6, p=0.15),
-        transforms.RandomEqualize(p=0.15),
-        transforms.RandomSolarize(128, p=0.1),
-        transforms.RandomInvert(p=0.05),
+        # transforms.RandomApply([transforms.ColorJitter(brightness=0.4, hue=0.2, contrast=0.5, saturation=0.2)], p=0.75),
+        # transforms.RandomPosterize(6, p=0.15),
+        # transforms.RandomEqualize(p=0.15),
+        # transforms.RandomSolarize(128, p=0.1),
+        # transforms.RandomInvert(p=0.05),
         transforms.RandomApply(
-            [transforms.ElasticTransform(alpha=random.random() * 200.0, sigma=8.0 + random.random() * 7.0)], p=0.75
+            [transforms.ElasticTransform(alpha=random.random() * 200.0, sigma=8.0 + random.random() * 7.0)], p=0.25
         ),
         RandomRotation(degrees=[-5, 5], p=0.75),
-        RandomResizedCrop(sizeHW, scale=(0.6, 1.6), ratio=(3.0 / 5.0, 2.0), p=0.9),
     ]
     train_compose = CustomCompose(
         [
@@ -52,6 +50,22 @@ def main(args):
             transforms.ConvertImageDtype(torch.float32),
             transforms.Normalize([0.5, 0.5, 0.5], [0.25, 0.25, 0.25]),
         ]
+    )
+    train_iter_compose = IterativeCustomCompose(
+        [
+            *argumentation_order_ls,
+            transforms.ConvertImageDtype(torch.float32),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.25, 0.25, 0.25]),
+        ],
+        transform_img_size=sizeHW,
+    )
+    test_iter_compose = IterativeCustomCompose(
+        [
+            RandomResizedCrop(sizeHW, scale=(0.6, 1.6), ratio=(3.0 / 5.0, 2.0), p=0.9),
+            transforms.ConvertImageDtype(torch.float32),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.25, 0.25, 0.25]),
+        ],
+        transform_img_size=sizeHW,
     )
     test_compose = CustomCompose(
         [
@@ -62,109 +76,120 @@ def main(args):
     )
 
     # dataloader
-    train_set, val_set, train_loader, val_loader = get_dataloader(
-        str(DatasetInfo.data_dir), Processing(train_compose), dataset_rate=0.8, batch_size=30, num_workers=24
-    )
+    train_loader, val_loader = get_dataloader(
+    filenames=[*get_filenames('Data/dataset_pickle/hit',specific_name="*.pickle"), *get_filenames('Data/dataset_pickle/miss',specific_name="*.pickle")[:7000]],
+    dataset_rate=0.8,
+    batch_size=15,
+    num_workers=36,
+    pin_memory=True,
+    usePickle=True,
+    compose=train_compose,
+    )[2:]
 
-    test_set, loader = get_test_dataloader(str(DatasetInfo.data_dir), Processing(test_compose), batch_size=30, num_workers=24)
-
+    # test_set, loader = get_test_dataloader(str(DatasetInfo.data_dir), Processing(test_compose), batch_size=65, num_workers=46)
 
     gpu0_bsz = 7
     acc_grad = 1
-    model = BadminationNet(in_seq=5, output_classes=32)
-    # model = models.efficientnet_v2_m(pretrained = True)
-    # model = models.resnet101(pretrained = True)
-    model = BalancedDataParallel(gpu0_bsz // acc_grad, model ,dim=0).to(device)
+    # File = '/root/Work/AI-CUP/out/loss937.04813.pt'
+    # model = BadminationNet(in_seq=5, output_classes=32)
+    model = Net(in_seq=5, output_classes=32)
 
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.w_d)
+    model = BalancedDataParallel(gpu0_bsz, model, dim=0).to(device)
+
+    # model.load_state_dict(torch.load(File))
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.w_d,momentum=args.m)
     train_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.step, gamma=args.gamma)
     total_epoch = args.epochs
     print_per_iteration = 100
-    save_path = './model/'
-    weight_path = ''
 
-    train_epoch_loss = []
-    val_epoch_loss = []
-    loss_func = CustomLoss().to(device)
-    for epoch in range(total_epoch):  # loop over the dataset multiple times
-        print('epoch = ', epoch)
+    loss_func = CustomLoss()
+    model_perform = ModelPerform(train_loss_ls=[], test_loss_ls=[])  # ,train_acc_ls=[],test_acc_ls=[])
 
-        # train
-        train_step_loss = []
+    total_epoch += model_perform.num_epoch
+    for model_perform.num_epoch in range(model_perform.num_epoch + 1, total_epoch + 1):  # loop over the dataset multiple times
+        print('epoch = ', model_perform.num_epoch)
+
+        #!train
+        best_val_loss = 10.0 
+        train_sum_loss = 0.0
         model.train()
         inputs: torch.Tensor
         labels: torch.Tensor
-        for i, (inputs, labels) in enumerate(tqdm(train_loader), 0):
+        for i, (inputs, labels) in enumerate(tqdm(train_loader)):
             # get the inputs; data is a list of [inputs, labels]
             inputs = inputs.to(device)
             labels = labels.to(device)
+
+            with torch.no_grad():
+                batch_coordXYs = torch.stack(
+                    [labels[:, 14:20:2], labels[:, 15:20:2]],
+                ).permute(
+                    1, 0, 2
+                )  # stack like: [[relatedX, ...], [relatedY, ...]]
+
+                inputs, batch_coordXYs = train_iter_compose(inputs, batch_coordXYs)
+                batch_coordXYs = batch_coordXYs.permute(1, 0, 2)
+                labels[:, 14:20:2] = batch_coordXYs[0]
+                labels[:, 15:20:2] = batch_coordXYs[1]
 
             optimizer.zero_grad()
             outputs = model(inputs)
             train_loss = loss_func(outputs, labels)
             train_loss.backward()
             optimizer.step()
+            train_sum_loss += train_loss.item()
+        model_perform.train_loss_ls.append(train_sum_loss / len(train_loader))
+        # model_perform.train_acc_ls.append(0.0)
 
-            train_step_loss.append(train_loss.item())
+        if (i + 1) % print_per_iteration == 0:
+            print(f'[{model_perform.num_epoch + 1:3}/{total_epoch}] loss: {model_perform.train_loss_ls[-1]:.5f}')
 
-            if (i + 1) % print_per_iteration == 0:
-                print(f'[ep {epoch + 1}][{i + 1:5d}/{len(train_loader):5d}] loss: {train_loss.item():.3f}')
-        train_epoch_loss.append(np.array(train_step_loss).mean())
-
-        train_scheduler.step()
+        # train_scheduler.step()
 
         # validation
+        sum_loss = 0
         model.eval()
         with torch.no_grad():
-            for inputs,labels in val_loader:
-                # val_step_loss=[]
-                inputs, labels = data
+            for inputs, labels in tqdm(val_loader):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
+
+                with torch.no_grad():
+                    batch_coordXYs = torch.stack(
+                        [labels[:, 14:20:2], labels[:, 15:20:2]]
+                    )  # stack like: [[relatedX, ...], [relatedY, ...]]
+
+                    inputs, batch_coordXYs = test_iter_compose(inputs, batch_coordXYs)
+                    labels[:, 14:20:2] = batch_coordXYs[0]
+                    labels[:, 15:20:2] = batch_coordXYs[1]
+
                 outputs = model(inputs)
+                # print(outputs)
                 val_loss = loss_func(outputs, labels)
                 sum_loss += val_loss.item()
-                # val_step_loss.append(val_loss.item())
 
-                if (i + 1) % print_per_iteration == 0:
-                    print('Validation Loss', sum_loss)
-            # val_epoch_loss.append(np.array(val_step_loss).mean())
+        model_perform.test_loss_ls.append(sum_loss / len(val_loader))
+        # model_perform.test_acc_ls.append(0.0)
 
-        correct = 0
-        total = 0
-        acc = 0
-        # test
-        if (epoch + 1) % total_epoch == 0:
-            with torch.no_grad():
-                for data in loader:
-                    images, labels = data
-                    images = images.to(device)
-                    labels = labels.to(device)
-                    # calculate outputs by running images through the network
-                    outputs = model(images)
-                    # the class with the highest energy is what we choose as prediction
-                    _, predicted = torch.max(outputs.data, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
-            print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.2f} %')
-            # print(type(correct),'  ',type(total))
-            acc = correct / total
-            weight_path = save_path + 'epochs_' + str(epoch + 1) + '_acc_' + str(acc) + '.pth'
-            print('weight_path = ', weight_path)
+        if best_val_loss > model_perform.test_loss_ls[-1]:
+            best_val_loss = model_perform.test_loss_ls[-1]
+            torch.save(model.state_dict(), f"out/best_loss{best_val_loss:.5f}.pt")
+        elif model_perform.num_epoch % 5 == 0:
+            torch.save(model.state_dict(), f"out/loss{model_perform.test_loss_ls[-1]:.5f}.pt")
 
-        # if (h_acc< acc) &(acc>0.65):
-        #     h_acc = acc
-        #     torch.save(model, weight_path)
+        model_perform.save_history_csv()
+        model_perform.draw_plot(0, model_perform.num_epoch)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # parser.add_argument('-net', type=str, required=True, help='net type')
     # parser.add_argument('-b', type=int, default=1024, help='batch size for dataloader')
-    parser.add_argument('-lr', type=float, default=0.004, help='initial learning rate')
+    parser.add_argument('-lr', type=float, default=0.01, help='initial learning rate')
     # parser.add_argument('-num_classes',type=int, default=32, help='classification number')
     parser.add_argument('-w_d', type=int, default=4e-5, help='weight_decay')
-    parser.add_argument('-epochs', type=int, default=10)
+    parser.add_argument('-epochs', type=int, default=25)
     parser.add_argument('-gamma', type=float, default=0.98, help='Initial gamma for scheduler and the default is 0.8.')
     parser.add_argument('-step', type=int, default=4, help='Initial step for scheduler and the default is 10.')
     parser.add_argument('-m', type=float, default=0.9, help='momentum')
